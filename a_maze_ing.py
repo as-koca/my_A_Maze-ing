9ca7cf65b34c830d6690b1b2e86603a557f2b6b9
KOCA Ali Sinan <kocaalisinan@okans-macbook-pro-2.home>
2026-07-01 01:41:00 +0200

    maze renders completely, changing colors - quiting - showing path and regnerating works, gotta fix 42 pattern walls in unperfect mode

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -8,13 +8,18 @@ from render import render_maze
 
 # Decided width of maze should be at least 10 for the 42 pattern to be included
 # Decided height should be at least 9 -> to have at least 2 cells around pattrn
+"""Can get a curses error if terminal window is smaller than maze bounds!"""
 
 
-def generate_maze(config: Config | None) -> tuple[Maze,
-                                                  list[str],
-                                                  set[tuple[int, int]]]:
+def generate_maze(config: Config | None,
+                  seed: int | None = None) -> tuple[Maze,
+                                                    list[str],
+                                                    set[tuple[int, int]],
+                                                    set[tuple[int, int]]]:
     """Game logic, carves and solves maze with given configuration and
-        writes output to a designated file."""
+        writes output to a designated file.
+
+        Returns: Maze, Solved Path, Pattern, Path Coords. In that order."""
 
     """Preliminary validation checks before game logic"""
     if not config:
@@ -22,8 +27,12 @@ def generate_maze(config: Config | None) -> tuple[Maze,
     pattern_fits: bool = True
     pattern_cells: set[tuple[int, int]] = set()
     maze = Maze(config.width, config.height)
-    if config.seed is None:
-        config.seed = int(time())
+    if seed is not None:
+        active_seed: int = seed
+    elif config.seed is not None:
+        active_seed = config.seed
+    else:
+        active_seed = int(time())
     if config.width < 10:
         print("Can not include '42' pattern into maze because of", end=' ')
         print("width (min: 10), Proceeding without it...")
@@ -42,10 +51,10 @@ def generate_maze(config: Config | None) -> tuple[Maze,
     while solved_path is None and carve_attempts < 100:
         maze = Maze(config.width, config.height)
         if config.perfect:
-            maze.carve_maze(config.entry, config.seed +
+            maze.carve_maze(config.entry, active_seed +
                             carve_attempts, pattern_cells)
         else:
-            maze.carve_unperfect_maze(config.entry, config.seed +
+            maze.carve_unperfect_maze(config.entry, active_seed +
                                       carve_attempts, pattern_cells)
         solved_path = maze.solve(config.entry, config.exit)
         carve_attempts += 1
@@ -57,16 +66,19 @@ def generate_maze(config: Config | None) -> tuple[Maze,
         print("after 100 attempts, Proceeding without it...")
         maze = Maze(config.width, config.height)
         if config.perfect:
-            maze.carve_maze(config.entry, config.seed + 100)
+            maze.carve_maze(config.entry, active_seed + 100)
         else:
-            maze.carve_unperfect_maze(config.entry, config.seed + 100)
+            maze.carve_unperfect_maze(config.entry, active_seed + 100)
         solved_path = maze.solve(config.entry, config.exit)
 
     if not solved_path:
         print("No path found to reach exit...")
         sys.exit()
-    print(f"Maze generated with seed: {config.seed + carve_attempts}")
-    return (maze, solved_path, pattern_cells)
+    else:
+        path_coords = maze.get_path_coords(solved_path, config.entry)
+
+    print(f"Maze generated with seed: {active_seed + carve_attempts}")
+    return (maze, solved_path, pattern_cells, path_coords)
 
 
 def main() -> None:
@@ -74,11 +86,11 @@ def main() -> None:
     config: Config | None = verify_file(parsed)
     if config is None:
         sys.exit()
-    maze, solved_path, pattern = generate_maze(config)
+    maze, solved_path, pattern, path_coords = generate_maze(config)
     write_maze(maze, config.entry, config.exit,
                solved_path, config.output_file)
     curses.wrapper(lambda stdscr: render_maze(
-        stdscr, maze, config, solved_path, pattern))
+        stdscr, maze, config, pattern, path_coords, generate_maze))
 
 
 if __name__ == "__main__":
--- render.py
+++ render.py
@@ -1,6 +1,8 @@
 import sys
 from maze_data import Maze
 from parser import Config
+from output import write_maze
+from collections.abc import Callable
 try:
     import curses
 except ModuleNotFoundError as e:
@@ -17,75 +19,141 @@ def is_even(n: int) -> bool:
     return False
 
 
-def draw_maze(maze: Maze, entry: tuple[int, int],
+def draw_maze(stdscr: curses.window,
+              maze: Maze,
+              entry: tuple[int, int],
               exit: tuple[int, int],
               pattern: set[tuple[int, int]],
               path_coords: set[tuple[int, int]],
-              show_path: bool = False) -> None:
+              show_path: bool = False,
+              palette: int = 0) -> None:
+    """
+        Draws every wall and cell of the maze with position-centric rendering.
+
+        - Checks if on a corner, horizontal or vertical wall, or a cell.
+        - Checks presence of wall by mapping position to maze coordinate then
+        looks in each direction for wall.
+        - Rendering done using empty blocks (space char) which are colored to
+        show a wall or passage.
+        - Screens `col` coordinate multiplied by 2 since we print 2 spaces,
+        and curses automatically prints onto col0 --> coln for 'n' characters.
+        """
+
+    wall_pair: int = 1 if palette == 0 else 3
+    path_pair: int = 2 if palette == 0 else 4
+    passage_pair: int = 8
+
     for row in range(2 * maze.height + 1):
         for col in range(2 * maze.width + 1):
             mx = col // 2
             my = (maze.height - 1) - (row // 2)
             if is_even(row) and is_even(col):
-                print("+", end='')
+                stdscr.addstr(row, col * 2, "  ", curses.color_pair(wall_pair))
             elif is_even(row):
                 above_y = my + 1
                 if 0 <= my < maze.height:
                     closed = maze.has_wall(mx, my, "N")
                 else:
                     closed = maze.has_wall(mx, above_y, "S")
-                print("---" if closed else "   ", end="")
+                pair: int = wall_pair if closed else passage_pair
+                stdscr.addstr(row, col * 2, "  ", curses.color_pair(pair))
             elif is_even(col):
                 left_x = mx - 1
                 if 0 <= mx < maze.width:
                     closed = maze.has_wall(mx, my, "W")
                 else:
                     closed = maze.has_wall(left_x, my, "E")
-                print("|" if closed else " ", end='')
+                pair = wall_pair if closed else passage_pair
+                stdscr.addstr(row, col * 2, "  ", curses.color_pair(pair))
             else:
                 if (mx, my) == entry:
-                    print("ENT", end='')
+                    pair = 5
                 elif (mx, my) == exit:
-                    print("EXI", end='')
+                    pair = 6
                 elif (mx, my) in pattern:
-                    print("###", end='')
+                    pair = 7
                 elif show_path and (mx, my) in path_coords:
-                    print(" o ", end='')
+                    pair = path_pair
                 else:
-                    print("   ", end="")
-        print("")
+                    pair = passage_pair
+                stdscr.addstr(row, col * 2, "  ", curses.color_pair(pair))
 
 
-maze = Maze(20, 15)
-patt = maze.make_pattern_anchors()
-maze.carve_maze((0, 0), seed=100, blocked=patt)
-path = maze.solve((0, 0), (19, 14))
-path_coords = maze.get_path_coords(path, (0, 0))
-
-draw_maze(maze, (0, 0), (19, 14), patt, path_coords, show_path=True)
-
-
-def render_maze(stdscr: curses.window, maze: Maze, config: Config,
-                path: list[str], pattern: set[tuple[int, int]]) -> None:
+def render_maze(stdscr: curses.window,
+                maze: Maze, config: Config,
+                pattern: set[tuple[int, int]],
+                path_coords: set[tuple[int, int]],
+                generate_maze: Callable) -> None:
     curses.start_color()
-    # palette 1
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)  # wall
-    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)  # path
-    # palette 2
-    curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)  # wall
-    curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_BLACK)  # path
+    # palette 1 (walls + path)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)    # wall
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_BLUE)     # path
+    # palette 2 (walls + path)
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_YELLOW)   # wall
+    curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_CYAN)     # path
     # fixed colors
-    curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK)  # Entry
-    curses.init_pair(6, curses.COLOR_RED, curses.COLOR_BLACK)  # Exit
-    curses.init_pair(7, curses.COLOR_MAGENTA,
-                     curses.COLOR_BLACK)  # 42 Pattern
+    curses.init_pair(5, curses.COLOR_BLACK, curses.COLOR_GREEN)    # entry
+    curses.init_pair(6, curses.COLOR_BLACK, curses.COLOR_RED)      # exit
+    curses.init_pair(7, curses.COLOR_BLACK, curses.COLOR_MAGENTA)  # 42 pattern
+    curses.init_pair(8, curses.COLOR_BLACK, curses.COLOR_BLACK)    # passage
 
-    stdscr.addstr(0, 0, "HIIIIIIIIIIIIIIIIIIII" + '\n')
+    curses.curs_set(0)
+
+    palette: int = 0
+    show_path: bool = False
     while True:
-        key = stdscr.getch()
-        if key == ord("q"):
-            break
+        stdscr.clear()
+        max_y, max_x = stdscr.getmaxyx()
+        needed_y = 2 * maze.height + 1
+        needed_x = (2 * maze.width + 1) * 2
+        if needed_y > max_y or needed_x > max_x:
+            stdscr.clear()
+            msg = "Terminal too small. Resize."
+            stdscr.addstr(0, 0, msg)
+            stdscr.refresh()
+            stdscr.getch()
+        else:
+            draw_maze(stdscr, maze,
+                      config.entry, config.exit,
+                      pattern, path_coords,
+                      show_path, palette)
+            stdscr.refresh()
+            key = stdscr.getch()
+            if key == ord("q"):
+                break
+            elif key == ord("c"):
+                palette = 1 - palette
+            elif key == ord("p"):
+                show_path = not show_path
+            elif key == ord("r"):
+                maze, solved_path, pattern, path_coords = generate_maze(
+                    config)
+                write_maze(maze, config.entry, config.exit,
+                           solved_path, config.output_file)
 
+
+# KEY_RESIZE when resizing the window gets passed to getch()
+
+if __name__ == "__main__":
+    maze = Maze(20, 15)
+    patt = maze.make_pattern_anchors()
+    maze.carve_maze((0, 0), seed=100, blocked=patt)
+    path = maze.solve((0, 0), (19, 14))
+    path_coords = maze.get_path_coords(path, (0, 0))
+
+    curses.wrapper(lambda stdscr: draw_maze(
+        stdscr, maze, (0, 0), (19, 14), patt, path_coords))
+
+
+# print("|" if closed else " ", end='')
+# print("+", end='')
+# print("---" if closed else "   ", end="")
+# print("|" if closed else " ", end="")
+# print("ENT", end='')
+# print("EXI", end='')
+# print("###", end='')
+# print(" o ", end='')
+# print("   ")
 
 # def draw_maze(maze: Maze, entry: tuple[int, int],
 #              exit: tuple[int, int],
